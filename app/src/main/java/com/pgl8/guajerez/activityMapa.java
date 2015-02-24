de6a34e1e91ddd12f2302e0aa723bbc6e25e39b0
Pgl8 <pepe.gl@gmail.com>
2015-02-24 13:14:39 +0100

    Cambios MapFragment

--- app/src/main/java/com/pgl8/guajerez/activityMapa.java
+++ app/src/main/java/com/pgl8/guajerez/activityMapa.java
@@ -1,13 +1,16 @@
 package com.pgl8.guajerez;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class activityMapa extends ActionBarActivity {
@@ -22,32 +25,23 @@ public class activityMapa extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
-        //mapView = (MapView) findViewById(R.id.map);
-        //mapView.onCreate(savedInstanceState);
-        //googleMap = mapView.getMap();
+        //Inicializamos el MapFragment
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        //mapFragment.getMapAsync((com.google.android.gms.maps.OnMapReadyCallback) this);
+
+        //Obtenemos el mapa y activamos localización
         googleMap = mapFragment.getMap();
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         googleMap.setMyLocationEnabled(true);
 
-        // Sets camera to location and zoom
-        /*Location mLocation = googleMap.getMyLocation();
-        LatLng actual = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(actual, 15));*/
-
+        //Activamos los elementos de la interfaz de usuario
         uiSettings = googleMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
         uiSettings.setAllGesturesEnabled(true);
-        /*Location mLocation = googleMap.getMyLocation();
-        double Lat = mLocation.getLatitude();
-        double Lon = mLocation.getLongitude();
-        Log.v(TAG, "Latitud"+Lat);
-        Log.v(TAG, "Longitud"+Lon);*/
 
-        /*LatLng coordinate = new LatLng(googleMap.getMyLocation().getLatitude(), googleMap.getMyLocation().getLongitude());
-        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 15);
-        googleMap.animateCamera(yourLocation);*/
+        //Centramos el mapa en la posicion del usuario NO FUNCIONA
+        Location location = googleMap.getMyLocation();
+        LatLng latlon = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdateFactory.newLatLng(latlon);
     }
 
     @Override
