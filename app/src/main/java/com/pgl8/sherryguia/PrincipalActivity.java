843c9097ee44d0b31671a007fabaf9385155407e
Pgl8 <pepe.gl@gmail.com>
2016-11-14 19:59:05 +0100

    Actualizada UI.

--- app/src/main/java/com/pgl8/sherryguia/PrincipalActivity.java
+++ app/src/main/java/com/pgl8/sherryguia/PrincipalActivity.java
@@ -65,8 +65,8 @@ public class PrincipalActivity extends AppCompatActivity implements GoogleApiCli
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
                 //TODO: Insert AR class activity.
                 //startActivity(new Intent(this, ));
             }
